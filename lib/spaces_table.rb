4de1b4885ba4c2f4f83d480127ec9d81bb49022d
winstoncwang <congwang@live.co.uk>
2022-06-01 12:41:20 +0100

    one-to-many relation users and spaces

--- lib/spaces_table.rb
+++ lib/spaces_table.rb
@@ -12,9 +12,9 @@ class SpacesTable
   end
 
   def add(space)
-    result = @db.run("INSERT INTO spaces (name, description, price, date_from, date_to) 
-    VALUES ($1, $2, $3, $4, $5) RETURNING id;",
-    [space.name, space.description, space.price, space.date_from, space.date_to])
+    result = @db.run("INSERT INTO spaces (name, description, price, date_from, date_to, user_id) 
+    VALUES ($1, $2, $3, $4, $5, $6) RETURNING id;",
+    [space.name, space.description, space.price, space.date_from, space.date_to, space.user_id])
     return result[0]["id"]
   end
 
@@ -22,9 +22,10 @@ class SpacesTable
     @db.run("DELETE FROM spaces WHERE id = $1;", [index])
   end
 
-  def update(index, name, description, price, date_from, date_to)
+  def update(index, new_space_entry)
     @db.run("UPDATE spaces SET name = $1, description = $2, price = $3, date_from = $4, date_to = $5 WHERE id = $6;",
-    [name, description, price, date_from, date_to, index])
+    [new_space_entry.name, new_space_entry.description, new_space_entry.price, 
+new_space_entry.date_from, new_space_entry.date_to, index])
   end
 
   def get(index)
@@ -32,7 +33,6 @@ class SpacesTable
     return row_to_object(result[0])
 
   end
-
 
   private
 
@@ -43,6 +43,7 @@ class SpacesTable
       row["price"].to_i,
       row["date_from"],
       row["date_to"],
+      row["user_id"].to_i,
       row["id"]
     )
   end
--- spec/model/space_spec.rb
+++ spec/model/space_spec.rb
@@ -2,20 +2,22 @@ require "space_entity"
 
 RSpec.describe SpaceEntity do
   it "Creates without an ID" do
-    space = SpaceEntity.new("Mazury", "beautiful house next to Mazury Lac", 100 , "01/01/2022", "05/01/2022")
+    space = SpaceEntity.new("Mazury", "beautiful house next to Mazury Lac", 100, "01/01/2022", "05/01/2022", 1)
     expect(space.name).to eq "Mazury"
     expect(space.description).to eq "beautiful house next to Mazury Lac"
     expect(space.price).to eq 100
     expect(space.date_from).to eq "01/01/2022"
     expect(space.date_to).to eq "05/01/2022"
+    expect(space.user_id).to eq 1
   end
   it "Creates with an ID" do
-    space = SpaceEntity.new("Mazury", "beautiful house next to Mazury Lac", 100 , "01/01/2022", "05/01/2022", 5)
+    space = SpaceEntity.new("Mazury", "beautiful house next to Mazury Lac", 100, "01/01/2022", "05/01/2022", 2, 5)
     expect(space.name).to eq "Mazury"
     expect(space.description).to eq "beautiful house next to Mazury Lac"
     expect(space.price).to eq 100
     expect(space.date_from).to eq "01/01/2022"
     expect(space.date_to).to eq "05/01/2022"
     expect(space.id).to eq 5
+    expect(space.user_id).to eq 2
   end
 end
